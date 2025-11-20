6a59d69861a4be8b2304b465ba1b05a9a54c1dce
mihil-lohana <mannthakkar12@gmail.com>
2025-11-21 00:20:38 +0530

    Insert sample data for Movies and Users tables

---
+++ Database/insert_sample_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO Movies (Title, Genre, Duration, Price)
+VALUES 
+('Avengers', 'Action', 180, 250),
+('Dunki', 'Comedy', 160, 200),
+('KGF 2', 'Action', 170, 300);
+
+INSERT INTO Users (FullName, Email)
+VALUES
+('Test User', 'test@example.com');
